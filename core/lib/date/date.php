803ab203477eeac8d59fefc9f36da0c47321f7d3
Hermen Reitsma <hajewiet@gmail.com>
2019-05-29 18:20:04 -0300

    Support timestamps in date lib

--- core/lib/date/date.php
+++ core/lib/date/date.php
@@ -4,7 +4,7 @@
  * @doc `[date input format]` outputs input date using [php format](http://php.net/manual/en/datetime.formats.php)
  */
 function date_sc($params) {
-    $ymd = get_param_value($params, 'date', current($params));
+    $date = get_param_value($params, 'date', current($params));
     $fmt = get_param_value($params, 'fmt', count($params) > 1? end($params) : 'd-m-Y');
-    return date($fmt, strtotime($ymd));
+    return date($fmt, is_numeric($date)? $date : strtotime($date));
 }
