edeff433553334ccffbf40ffbc40fb50641ee17e
Hermen Reitsma <hajewiet@gmail.com>
2019-06-20 10:48:50 -0300

    Update execution time and mem. usage libs

--- core/modules/admin/lib/execution-time/execution-time.php
+++ core/modules/admin/lib/execution-time/execution-time.php
@@ -1,5 +1,9 @@
 <?php
 
 function execution_time_sc() {
-	echo sprintf("%01.3fs", microtime(true) - $GLOBALS['SYSTEM']['request_time']);
+	echo execution_time();
+}
+
+function execution_time() {
+	return sprintf("%01.3fs", microtime(true) - $GLOBALS['SYSTEM']['request_time']);
 }
