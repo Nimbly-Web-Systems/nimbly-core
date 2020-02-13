5c34e2610f44b442fcc577012106aad4779dcba8
Hermen Reitsma <hajewiet@gmail.com>
2020-02-13 19:12:47 -0300

    Do not include port 443 for https redirects

--- core/lib/url/url.php
+++ core/lib/url/url.php
@@ -19,7 +19,7 @@ function url_absolute($relative_url) {
     return sprintf("%s://%s%s%s%s", 
             empty($_SERVER['HTTPS'])? "http" : "https",
             $_SERVER['SERVER_NAME'],
-            $_SERVER['SERVER_PORT'] !== '80'? ":" . $_SERVER['SERVER_PORT'] : "",
+            $_SERVER['SERVER_PORT'] !== '80' && $_SERVER['SERVER_PORT'] !== 443? ":" . $_SERVER['SERVER_PORT'] : "",
             $GLOBALS['SYSTEM']['uri_base'],
             $relative_url);
 }
