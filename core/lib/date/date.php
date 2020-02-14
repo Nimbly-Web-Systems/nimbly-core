591b9b3969154aa5d52999e012045ddf4e6dd4c1
Hermen Reitsma <hajewiet@gmail.com>
2020-02-14 11:12:23 -0300

    [date]: defaults to current time

--- core/lib/date/date.php
+++ core/lib/date/date.php
@@ -4,7 +4,12 @@
  * @doc `[date input format]` outputs input date using [php format](http://php.net/manual/en/datetime.formats.php)
  */
 function date_sc($params) {
-    $date = get_param_value($params, 'date', current($params));
+	if (count($params) === 1 && !isset($params['fmt'])) {
+		$default_date = current($params);
+	} else {
+		$default_date = time();
+	}
+    $date = get_param_value($params, 'date', $default_date);
     $fmt = get_param_value($params, 'fmt', count($params) > 1? end($params) : 'd-m-Y');
     return date($fmt, is_numeric($date)? $date : strtotime($date));
 }
