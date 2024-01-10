12b3303c0e511622de48579ce059f54d1989e69f
Hermen Reitsma <hajewiet@gmail.com>
2024-01-09 22:42:16 -0300

    Support legacy lazy loading images in editable content

--- core/lib/get-html/get-html.php
+++ core/lib/get-html/get-html.php
@@ -4,26 +4,48 @@ load_library('lookup');
 load_library('util');
 function get_html_sc($params)
 {
-    $resource_set = dot2rs(current($params));
-    if (!$resource_set) {
-        return;
-    }
-    $resource = $resource_set[0];
-    $uuid = $resource_set[1];
-    $field = $resource_set[2];
+    $var = current($params);
+    if (isset($GLOBALS['SYSTEM']['variables'][$var])) {
+        $html = $GLOBALS['SYSTEM']['variables'][$var];
+    } else {
+        $resource_set = dot2rs(current($params));
+        if (!$resource_set) {
+            return;
+        }
+        $resource = $resource_set[0];
+        $uuid = $resource_set[1];
+        $field = $resource_set[2];
 
-    if (!data_exists($resource)) {
-        data_create_resource($resource, ['fields' => false]);
+        if (!data_exists($resource)) {
+            data_create_resource($resource, ['fields' => false]);
+        }
+
+        if (!data_exists($resource, $uuid)) {
+            data_create($resource, $uuid, []);
+            $default = get_param_value($params, 'default', '');
+            echo $default;
+            return;
+        }
+        $html = lookup_data($resource, $uuid, $field, get_param_value('default', ''));
     }
 
-    if (!data_exists($resource, $uuid)) {
-        data_create($resource, $uuid, []);
-        $default = get_param_value($params, 'default', '');
-        echo $default;
-        return;
+    // replace legacy lazy loading images
+    $legacy_img_sizes = get_param_value($params, 'legacy-img-sizes');
+    if (!empty($legacy_img_sizes)) {
+        load_library('get-img-html', 'images');
+        $html = preg_replace_callback(
+            '/<img src="data:image\/gif;base64,R0lGODl[^=]+==" data-img-uuid="([0-9a-f]+)"[^>]+>/i',
+            function($matches) use ($legacy_img_sizes) {
+                return get_img_html_sc([
+                    'uuid' => $matches[1], 
+                    'sizes' => $legacy_img_sizes,
+                    'class' => 'w-full bg-neutral-100'
+                    ]);
+            },
+            $html
+        );
     }
 
-    $html = lookup_data($resource, $uuid, $field, get_param_value('default', ''));
     $result = strip_tags($html, "<h1><h2><h3><h4><h5><h6><b><strong><a><i><p><blockquote><ol><ul><li><br><img><iframe><figure><video><source>");
     echo $result;
 }
