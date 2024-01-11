743f1d2f6ff98b6a6b24b6b10fd757b379d54374
Hermen Reitsma <hajewiet@gmail.com>
2024-01-11 13:43:59 -0300

    Update nop documentation

--- core/lib/nop/nop.php
+++ core/lib/nop/nop.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * @doc [nop] does nothing (no operation). Can be used to disable a shortcode in your template: `[nop ipsum words=5000]` does nothing
+ * @doc [nop] does nothing (no operation). Can be used for comments or to temporarily disable a shortcode in your template: 
+ * `[#nop ipsum words=5000#]` does nothing
+ * `[#nop <!-- i'm a comment that's only visible to developers --> #]`
  */ 
 function nop_sc($params) {
     return; 
