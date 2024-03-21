fe21b5e8f60e7932235d3b70a580c9763f42cddd
Hermen Reitsma <hajewiet@gmail.com>
2024-03-21 19:57:58 -0300

    Rename lib first to get-first

--- core/lib/get-first/get-first.php
+++ core/lib/get-first/get-first.php
@@ -4,14 +4,12 @@
  * @doc * `[first dataset]` returns first item from dataset, stored in variable named `first`
  * @doc * `[first dataset var=varname]` returns first item from dataset, stored in variable named `varname`
  */
-function first_sc($params) {
+function get_first_sc($params) {
     load_library('get');
     load_library('set');
     $data_id = current($params);
-    $var_id = get_param_value($params, "var", "first") . '.';
-
+    $var_id = get_param_value($params, "var", "first");
     $data = get_variable($data_id);
-
     if (!is_array($data) && is_string($data)) {
         $data = array($data);
     }
@@ -21,7 +19,6 @@ function first_sc($params) {
     if (count($data) < 1) {
         return;
     }
-
     $item = current($data);
     if (is_string($item)) {
         set_variable($var_id, $item);
