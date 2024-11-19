1a56dcaf2ba5646bf52513b4bf12e6489726748a
Hermen Reitsma <hajewiet@gmail.com>
2024-11-19 14:53:27 -0300

    Use tld for language detection

--- core/lib/detect-language/detect-language.php
+++ core/lib/detect-language/detect-language.php
@@ -28,13 +28,19 @@ function detect_language_sc() {
     if (!empty($lang) && strlen($lang) === 2 && in_array($lang, $ALLOWED_LANG)) {
         return $lang;
     }
+
+    //3. from top level domain
+    $tld = end(explode('.', $_SERVER['SERVER_NAME']));
+    if (!empty($tld) && in_array($tld, $ALLOWED_LANG)) {
+        return $tld;
+    } 
     
-    //3. from browser language
+    //4. from browser language
     $browser_language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'en', 0, 2);
     if (in_array($browser_language, $ALLOWED_LANG)) {
         return $browser_language;
     }
     
-    //4. default to 'en' if any other method failed
+    //5. default to 'en' if any other method failed
     return "en";
 }
