a67ac7145a31ec6b2a62307312f27a716e6fb64c
Hermen Reitsma <hajewiet@gmail.com>
2025-04-28 09:27:46 -0300

    Allow inline editing for i18n resources

--- core/lib/detect-language/detect-language.php
+++ core/lib/detect-language/detect-language.php
@@ -3,6 +3,11 @@
 
 
 function detect_language_sc() {
+    static $result = false;
+	if (!empty($result)) {
+		return $result;
+	}
+
     load_library('lookup');
     $allowed_lang = lookup_data('.config', 'site', 'languages', ['en']);
     //1. from url
@@ -26,22 +31,26 @@ function detect_language_sc() {
     load_library('get');
     $lang = get_variable('lang');
     if (!empty($lang) && strlen($lang) === 2 && in_array($lang, $allowed_lang)) {
-        return $lang;
+        $result = $lang;
+        return $result;
     }
 
     //3. from top level domain
     $server_name_parts = explode('.', $_SERVER['SERVER_NAME']);
     $tld = end($server_name_parts);
     if (!empty($tld) && in_array($tld, $allowed_lang)) {
-        return $tld;
+        $result = $tld;
+        return $result;
     } 
     
     //4. from browser language
     $browser_language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'en', 0, 2);
     if (in_array($browser_language, $allowed_lang)) {
-        return $browser_language;
+        $result = $browser_language;
+        return $result;
     }
     
-    //5. default to 'en' if any other method failed
-    return "en";
+    //5. default to first language or en if any other method failed
+    $result = current($allowed_lang) ?? 'en';
+    return $result;
 }
