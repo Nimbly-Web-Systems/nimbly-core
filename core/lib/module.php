a1d9831878ca61d161cc61a0d4d20c1fcdeb34a5
Hermen Reitsma <hajewiet@gmail.com>
2026-08-06 21:32:44 -0300

    feat: make module:install Composer-aware

--- core/lib/module.php
+++ core/lib/module.php
@@ -11,3 +11,13 @@ function module_sc($params) {
     }
     return null;
 }
+
+/**
+ * Whether a module's install step (php core/cli/nimbly.php module:install <name>)
+ * has been run in this environment. Backed by the .state resource, the same
+ * key-value pattern already used for scheduler and fatal-alert state.
+ */
+function module_is_installed($name) {
+    load_library('data');
+    return data_exists('.state', 'module:' . $name);
+}
